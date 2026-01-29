5e6dc6285096e09d5fc4a0639be39fbc254edb3d
JoelMcEvoy-Swift <Joel@mcevoyswift.com>
2026-01-29 22:59:30 +0000

    Update server.js

--- server.js
+++ server.js
@@ -142,6 +142,9 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("Socket.IO server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Socket.IO server running on port ${PORT}`);
 });
+
